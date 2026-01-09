afd8b295b07909fc3aa3aa57ff8111f8a03090c8
Myltiane Alves <myltiane.aux@gmail.com>
2026-01-09 16:40:19 -0300

    reafactor:

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-// const BASE_URL = "https://api-node-quality.vercel.app/";
+const BASE_URL = "https://api-node-quality.vercel.app/";
 //  const BASE_URL = "http://localhost:6001";
 
 const axiosInstance = axios.create({
