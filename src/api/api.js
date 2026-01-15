0b6148e8555f0401cab6a22fe8cd86d7506ae1f1
ColaboradorGTO <114693668+ColaboradorGTO@users.noreply.github.com>
2026-01-15 09:58:25 -0300

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const BASE_URL = "https://api-node-quality.vercel.app/";
-  // const BASE_URL = "http://localhost:6001";
+// const BASE_URL = "http://localhost:6001";
 
 const axiosInstance = axios.create({
   timeout: 100000,
