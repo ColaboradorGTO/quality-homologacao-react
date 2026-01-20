fea92fe22d22c27631553dfee3035cdadf97b5c2
LucasEutacio <lucas123@gmail.com>
2026-01-20 17:15:23 -0300

    feat: ajuste no relatorio de BI ajuste no nome do parametro

--- src/componets/Gerencia/Components/ActionBI/actionRelatorioBI.jsx
+++ src/componets/Gerencia/Components/ActionBI/actionRelatorioBI.jsx
@@ -35,7 +35,7 @@ export const ActionRelatorioBI = () => {
   const getListaRelatorioBI = async () => {
     if (usuarioLogado && usuarioLogado.IDEMPRESA) {
       try {
-        const response = await get(`/relatorioBI?idEmpresa=${usuarioLogado?.IDEMPRESA}&idRelatorio=1`);
+        const response = await get(`/relatorioBI?idEmpresaLogin=${usuarioLogado?.IDEMPRESA}&idRelatorio=1`);
 
         if (response.data) {
           setDadosBI(response.data);
