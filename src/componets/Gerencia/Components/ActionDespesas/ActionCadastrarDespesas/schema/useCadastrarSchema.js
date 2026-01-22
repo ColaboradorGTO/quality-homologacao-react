daf45d90a378b3b21054babb6afaa47bb68c1208
LucasEutacio <lucas123@gmail.com>
2026-01-22 11:01:25 -0300

    feat: ajuste no schema do select do tipo de despesa da modal cadastrar despesa na tela de despesas

--- src/componets/Gerencia/Components/ActionDespesas/ActionCadastrarDespesas/schema/useCadastrarSchema.js
+++ src/componets/Gerencia/Components/ActionDespesas/ActionCadastrarDespesas/schema/useCadastrarSchema.js
@@ -1,7 +1,22 @@
 import * as yup from "yup";
-export const schema = yup.object({
-    // empresaDespesa: yup.string().required("O campo Empresa é obrigatório."),
-    historicoDespesa: yup.string().required("O campo Histórico é obrigatório."),
-    dsPagoDespesa: yup.string().required("O campo Histórico é obrigatório."),
-    valorDespesa: yup.string().required("O campo Valor Despesa é obrigatório."),
+export const schema = yup.object().shape({
+    //empresaDespesa: yup.string().required("O campo Empresa é obrigatório."),
+    tipoDespesaSelecionada: yup
+        .mixed()
+        .nullable()
+        .test("tipoDespesaSelecionada", "despesas é obrigatório", (v) => {
+            return !!v && typeof v === "object" && !!v.value;
+        }),
+
+    historicoDespesa: yup
+        .string()
+        .required("O campo Histórico é obrigatório."),
+
+    dsPagoDespesa: yup
+        .string()
+        .required("O campo Histórico é obrigatório."),
+
+    valorDespesa: yup
+        .string()
+        .required("O campo Valor Despesa é obrigatório."),
 })
